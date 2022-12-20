b5ac7fa2f9880bd57991d0d2c0747366a0470b69
NicolasDenier <nicodenier@laposte.net>
2022-12-20 19:06:09 +0100

    Oauth and google analytics api

--- first_micro/main.py
+++ first_micro/main.py
@@ -1,8 +1,43 @@
-from flask import Flask, request, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, url_for, request
 from markupsafe import escape # to escape user inputs and so avoid injection attacks
-
+import requests
+import json
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
 
 app = Flask(__name__)
+app.config.from_pyfile('settings.py')
+
+flow = None
+
+def create_keyfile_dict():
+    variables_keys = {
+        "web":{
+        "client_id": app.config.get("CLIENT_ID"),
+        "project_id": app.config.get("PROJECT_ID"),
+        "auth_uri": app.config.get("AUTH_URI"),
+        "token_uri": app.config.get("TOKEN_URI"),
+        "auth_provider_x509_cert_url": app.config.get("AUTH_PROVIDER_X509_CERT_URL"),
+        "client_secret": app.config.get("CLIENT_SECRET")
+        }
+    }
+    return variables_keys
+
+
+def ga_auth(scopes):
+    global flow
+    auth_url="/"
+    try:
+        flow = InstalledAppFlow.from_client_config(create_keyfile_dict(), scopes)
+        flow.redirect_uri = 'https://lhkxlc.deta.dev/cookies'
+        auth_url, _ = flow.authorization_url(prompt='consent')
+        print('Please go to this URL: {}'.format(auth_url))
+    except Exception as e:
+        print('exception')
+        print(e)
+
+    return '{}'.format(auth_url)
+
 
 prefix_google="""
     <!-- Google tag (gtag.js) -->
@@ -19,11 +54,7 @@ user_input=""
 
 @app.route('/', methods=["GET"])
 def hello_world():
-    title="<h1>Hello</h1>"
-    details="<p>You are analyzed by Google Analytics</p>"
-    button=f"<a href='{url_for('click')}'>click here</a><br>"
-    button+=f"<a href='{url_for('logger')}'>see logs</a>"
-    display=prefix_google+title+details+button
+    display=prefix_google+render_template('home.html')
     return display
 
 @app.route('/click', methods=["GET"])
@@ -35,8 +66,43 @@ def logger():
     global user_input
     print('This is a log from python')
     if request.method == 'POST':
-        user_input+=escape(request.form.get("user_input"))
-        user_input+='\n'
-    else:
-        user_input+='\n'
+        user_input=escape(request.form.get("user_input")) + '\n' + user_input
     return prefix_google+render_template('logger.html', text=user_input)
+
+
+@app.route('/cookies/auth/', methods=["GET"])
+def auth():
+    scopes = ['https://www.googleapis.com/auth/analytics.readonly']
+    auth_url = ga_auth(scopes)
+    return redirect(auth_url)
+
+
+@app.route('/cookies/', methods=["GET"])
+def cookies():
+    code = escape(request.args.get('code', None))
+    #state  = request.args.get('state', None)
+    flow.fetch_token(code=code)
+    return redirect(url_for('visitors'))
+
+
+@app.route('/cookies/visitors', methods=["GET"])
+def visitors():
+    google_cookies = "Unable to get cookies"
+    try:
+        req = requests.get("https://www.google.com/")
+        google_cookies = req.cookies.get_dict()
+        google_cookies = str(json.dumps(google_cookies, indent=2))
+
+        service = build('analytics', 'v3', credentials=flow.credentials)
+        results = service.data().ga().get(
+            ids='ga:' + app.config.get("VIEW_ID"),
+            start_date='30daysAgo',
+            end_date='today',
+            metrics='ga:users'
+            ).execute()
+        number_users = results['totalsForAllResults']['ga:users']
+    except Exception as e:
+        print('exception')
+        print(e)
+    display=prefix_google+render_template('cookies.html', google_cookies=google_cookies, number_users=str(number_users))
+    return display
---
+++ first_micro/settings.py
@@ -0,0 +1,9 @@
+from os import environ 
+
+CLIENT_ID=environ.get('CLIENT_ID')
+PROJECT_ID=environ.get('PROJECT_ID')
+AUTH_URI=environ.get('AUTH_URI')
+TOKEN_URI=environ.get('TOKEN_URI')
+AUTH_PROVIDER_X509_CERT_URL=environ.get('AUTH_PROVIDER_X509_CERT_URL')
+CLIENT_SECRET=environ.get('CLIENT_SECRET')
+VIEW_ID=environ.get('VIEW_ID')
